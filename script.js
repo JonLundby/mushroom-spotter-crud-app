b32a28fc686b00b7a49ad021aea070a5e69b880a
Jon <lundbyjon@hotmail.com>
2023-06-28 19:17:55 +0200

    database endpoint setup, fetch & show + css color variables

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 "use strict";
 
+const endpoint = "https://mushroom-spotter-data-default-rtdb.europe-west1.firebasedatabase.app/";
+let posts;
+
 window.addEventListener("load", startApp);
 
-function startApp() {
-    console.log("running...");
+async function startApp() {
+  posts = await getposts();
+  console.log(posts)
+
+  //posts.forEach(generatePost);
+  showSpottersPosts(posts);
+}
+
+async function getposts() {
+  const response = await fetch(`${endpoint}/post.json`);
+  const data = await response.json();
+  if (typeof data === "object" && data !== null) {
+    const posts = preparePosts(data);
+    return posts;
+  } else {
+    throw new Error("Invalid, data should be an object");
+  }
+}
+
+function preparePosts(data) {
+  const postsArr = [];
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const post = data[key];
+      post.id = key;
+      postsArr.push(post);
+    }
+  }
+  return postsArr;
+}
+
+function showSpottersPosts(posts) {
+    document.querySelector("#spotter-posts-container").innerHTML = "";
+  for (const post of posts) {
+    console.log(post);
+      generatePost(post);
+    }
+}
+
+function generatePost(postObject) {
+  const htmlPost = /*html*/ `
+                        <article>
+                            <h2>${postObject.mushroomname}<h2>
+                            <h3>${postObject.namelatin}</h3>
+                            <img src=${postObject.image}>
+                            <p>${postObject.areafound}</p>
+                        </article>
+    `;
+
+  document.querySelector("#spotter-posts-container").insertAdjacentHTML("beforeend", htmlPost);
 }
